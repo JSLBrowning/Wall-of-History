ca4982d70f292f563035097a7ab101162f4e4cf8
James <james@wallofhistory.com>
2021-01-13 20:39:59 -0600

    Create populate.php

---
+++ php/populate.php
@@ -0,0 +1,6 @@
+<?php
+    // Some of the pages that require PHP — such as /read and /reference — are very messy right now, on account of the fact that all the PHP necessary to populate them with content is actually in those pages.
+    // As such, all of this code will be relocated here, cleaned up, and streamlined.
+    // The aforementioned pages will henceforth call the functions defined here.
+    // https://stackoverflow.com/questions/8104998/how-to-call-function-of-one-php-file-from-another-php-file-and-pass-parameters-t
+?>
