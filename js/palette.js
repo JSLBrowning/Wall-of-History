5b752622d06166b7954d769908f12edefe171c57
James <james@wallofhistory.com>
2021-07-27 19:09:16 -0500

    Update palette swap mechanics

--- js/palette.js
+++ js/palette.js
@@ -1,14 +1,82 @@
-function swapPalettes() {
-    console.log("Swapping palettes…");
-    const root = document.documentElement;
-    root.classList.add('white');
+function correct() {
+    if (localStorage.getItem("colorScheme") === "light") {
+        document.documentElement.classList.toggle('light');
+        document.getElementById("paletteSwapButton").innerHTML = "☽";
+        try {
+            $(".twitter-timeline").attr("data-theme", "light");
+        } catch (error) {
+            console.log("No Twitter timeline is embedded on this page.");
+        }
+    }
 
-    document.documentElement.classList.toggle('light');
-    root.classList.toggle('black');
+    if (localStorage.getItem("fontSize") != null) {
+        let fontSize = localStorage.getItem("fontSize");
+        switch (fontSize) {
+            case "smallest":
+                document.documentElement.classList.toggle('smallest');
+                break;
+            case "smaller":
+                document.documentElement.classList.toggle('smaller');
+                break;
+            case "small":
+                document.documentElement.classList.toggle('small');
+                break;
+            case "big":
+                document.documentElement.classList.toggle('big');
+                break;
+            case "bigger":
+                document.documentElement.classList.toggle('bigger');
+                break;
+            case "biggest":
+                document.documentElement.classList.toggle('biggest');
+                break;
+        }
+    }
 }
 
-// https://academind.com/tutorials/adding-dark-mode/
-// https://codepen.io/fiszer/pen/MVoPdL
+correct();
+
+function swapPalettes() {
+    if (localStorage.getItem("colorScheme") === "light") {
+        localStorage.setItem("colorScheme", "dark");
+        document.documentElement.classList.toggle('light');
+        document.getElementById("paletteSwapButton").innerHTML = "☀";
+    } else {
+        localStorage.setItem("colorScheme", "light");
+        document.documentElement.classList.toggle('light');
+        document.getElementById("paletteSwapButton").innerHTML = "☽";
+    }
+
+    if (window.location.pathname.includes("about")) {
+        location.reload();
+    }
+}
+
+function increaseFontSize() {
+    let currentFontSize = localStorage.getItem("fontSize");
+    document.documentElement.classList.remove("smallest", "smaller", "small", "big", "bigger", "biggest");
+
+    switch (fontSize) {
+        case "smallest":
+            document.documentElement.classList.toggle('smallest');
+            break;
+        case "smaller":
+            document.documentElement.classList.toggle('smaller');
+            break;
+        case "small":
+            document.documentElement.classList.toggle('small');
+            break;
+        case "big":
+            document.documentElement.classList.toggle('big');
+            break;
+        case "bigger":
+            document.documentElement.classList.toggle('bigger');
+            break;
+        case "biggest":
+            document.documentElement.classList.toggle('biggest');
+            break;
+    }
+}
 
 /**
 body, header, main bg: #161616
