906ac4bf67824b275ad4c2d8950495c3496d1f3f
James <james@wallofhistory.com>
2021-12-05 18:01:14 -0600

    Begin upgrading modal functionality

--- js/modal.js
+++ js/modal.js
@@ -1,121 +1,27 @@
-// MENU AND SETTINGS MODALS
-var allModals = document.getElementsByClassName("modal");
-var navigationButton = document.getElementById("navigationButton");
-var navigationModal = document.getElementById("navigationModal");
-var navigationClose = document.getElementById("navigationClose");
-var settingsButton = document.getElementById("settingsButton");
-var settingsModal = document.getElementById("settingsModal");
-var settingsClose = document.getElementById("settingsClose");
-
-navigationButton.onclick = function() {
-    settingsButton.style.zIndex = "-1";
-    // Settings button is still at 1 a second too late.
-    navigationModal.style.display = "block";
-}
-
-navigationClose.onclick = function() {
-    navigationModal.style.display = "none";
-}
-
-if (settingsModal) {
-    settingsButton.onclick = function() {
-        settingsModal.style.display = "block";
+function toggleModal(id) {
+    let targetModal = document.getElementById(id);
+    let targetModalContent = document.getElementById(id).children[0];
+    if (targetModal.style.visibility == "hidden" || targetModal.style.visibility == "") {
+        targetModal.style.backgroundColor = "rgba(0,0,0,0.75)";
+        targetModalContent.classList.add("modal-content-left-visible");
+        targetModal.style.visibility = "visible";
+        targetModal.style.zIndex = "3";
+    } else {
+        setTimeout(function() { targetModal.style.visibility = "hidden" }, 500);
+        targetModalContent.classList.remove("modal-content-left-visible");
+        targetModal.style.backgroundColor = "rgba(0,0,0,0)";
+        targetModal.style.zIndex = "0";
     }
 }
 
-if (settingsModal) {
-    settingsClose.onclick = function() {
-        settingsModal.style.display = "none";
-    }
-}
-
-// This works for the second modal, but not the first... Why?
-// Well, now it doesn't work for either of them. Great.
-// Wait, it works for... the first one, on the homepage.
 window.onclick = function(event) {
     if (event.target.classList.contains('modal')) {
-        for (i = 0; i < allModals.length; i++) {
-            allModals[i].style.display = "none";
-            settingsButton.style.zIndex = "1";
+        let modals = document.getElementsByClassName("modal");
+        for (let i = 0; i < modals.length; i++) {
+            setTimeout(function() { modals[i].style.visibility = "hidden" }, 500);
+            modals[i].children[0].classList.remove("modal-content-left-visible");
+            modals[i].style.backgroundColor = "rgba(0,0,0,0)";
+            modals[i].style.zIndex = "0";
         }
-    }
-}
-
-let referenceItems = localStorage.getItem("referenceTerms").split(",");
-
-// Next thing. Add a regex for spaces and punctuation to fix the Makuta/Maku error on eba6c8.
-for (i = 0; i < referenceItems.length; i++) {
-    $("p:contains('" + referenceItems[i] + " ')").html(function(_, html) {
-        regex = new RegExp(referenceItems[i] + " ", "gi");
-        return html.replace(regex, '<a data-reference="' + referenceItems[i] + '" onclick="getModalContent(this)" style="cursor: pointer;">' + referenceItems[i] + '</a> ');
-    });
-
-    $("p:contains('" + referenceItems[i] + ",')").html(function(_, html) {
-        regex = new RegExp(referenceItems[i] + ",", "gi");
-        return html.replace(regex, '<a data-reference="' + referenceItems[i] + '" onclick="getModalContent(this)" style="cursor: pointer;">' + referenceItems[i] + '</a>,');
-    });
-
-    /* This one creates problems for some reason. See “Onua — Toa of Earth,” line 22.
-    $("p:contains('" + referenceItems[i] + ".')").html(function(_, html) {
-        regex = new RegExp(referenceItems[i] + ".", "gi");
-        return html.replace(regex, '<a data-reference="' + referenceItems[i] + '" onclick="getModalContent(this)" style="cursor: pointer;">' + referenceItems[i] + '</a>.');
-    }); */
-
-    $("p:contains('" + referenceItems[i] + "…')").html(function(_, html) {
-        regex = new RegExp(referenceItems[i] + "…", "gi");
-        return html.replace(regex, '<a data-reference="' + referenceItems[i] + '" onclick="getModalContent(this)" style="cursor: pointer;">' + referenceItems[i] + '</a>…');
-    });
-
-    $(".contentsText p").find("a").contents().unwrap();
-}
-
-// Get the modal
-let modal = document.getElementById("myModal");
-
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-function getModalContent(identifier) {
-    getContent($(identifier).data('reference'));
-}
-
-function getContent(str) {
-    if (str.length == 0) {
-        document.getElementById("modal-data").innerHTML = "";
-        return;
-    } else {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("modal-data").innerHTML = this.responseText;
-
-                spoilerlevel = parseInt(localStorage.getItem("WallofHistorySpoilerLevel"));
-                children = document.getElementById("modal-data").children;
-                for (i = 0; i < children.length; i++) {
-                    if (children[i].hasAttribute("data-spoiler")) {
-                        if (parseInt(children[i].getAttribute("data-spoiler")) > spoilerlevel) {
-                            children[i].style.display = "none";
-                        }
-                    }
-                }
-            }
-        };
-        xmlhttp.open("GET", "../php/getmodaldata.php?q=" + str, true);
-        xmlhttp.send();
-    }
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-    document.getElementById("modal-data").innerHTML = "<p>No information is available at this time.</p>";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-        document.getElementById("modal-data").innerHTML = "<p>No information is available at this time.</p>";
     }
 }
