c9c7412f6cb8ff5ecc324c1771aa1eeff7b0114e
James <james@wallofhistory.com>
2022-01-26 23:04:16 -0600

    Combine main.js and /mnl/main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
+/***********************
+ * UNIVERSAL FUNCTIONS *
+ ***********************/
+
+/* This function loads the configuration data from config.json. */
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', '/config/config.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+
 async function initialize() {
+    /* Add code to use loadJSON and store config data in localStorage. */
     if (localStorage.getItem("WallofHistorySavePlace") != null) {
         let oldSavePlace = localStorage.getItem("WallofHistorySavePlace");
         localStorage.clear();
@@ -87,18 +106,6 @@ async function initialize() {
     }
 };
 
-function loadJSON(callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', '/config/config.json', true);
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
-
 function getLanguageList() {
     return new Promise(resolve => {
         var xmlhttp = new XMLHttpRequest();
@@ -145,7 +152,9 @@ async function resetReader() {
     }
 }
 
-/* READER NAVIGATION HELPERS */
+/*
+ * READER NAVIGATION HELPERS
+ */
 
 function getOptimalLanguage(combo) {
     return new Promise(resolve => {
@@ -172,6 +181,27 @@ function getOptimalLanguage(combo) {
     });
 }
 
+/* LANGUAGE STUFF (INCOMPLETE) */
+
+let full = localStorage.getItem("languageList").split(",");
+let available = document.getElementsByTagName("section");
+
+if (available.length >= 1) {
+    let a = []
+    for (i = 0; i < available.length; i++) {
+        a.push(available[i].lang);
+    }
+
+    let intersection = full.filter(x => a.includes(x));
+
+    $("section:lang(" + intersection[0] + ")").css("display", "block");
+}
+
+/*********************************************************
+ * CHRONOLOGY-BASED READER NAVIGATION FUNCTIONS (WALL OF HISTORY VERSIONS) *
+ *********************************************************/
+
+/* This function finds the index of the current page in the active reading order. */
 function findSelf() {
     let readingOrder = localStorage.getItem("readingOrder:" + sessionStorage.getItem("activeReadingOrder")).split(",");
     let index, result;
@@ -186,8 +216,8 @@ function findSelf() {
     return (result);
 }
 
+/* This function returns the indexes of the first and last selected pages in the active reading order. */
 function filteredSelf(readingOrder) {
-    // This returns the indexes of the first and last active pages in the reading order.
     let index;
     let returns = [];
     for (index = 0; index < readingOrder.length; index++) {
@@ -200,7 +230,52 @@ function filteredSelf(readingOrder) {
     return ([returns[0], returns[returns.length - 1]]);
 }
 
-function showButtons() {
+/* This function takes an ID, version, and language (optional) and jumps straight to that page. */
+async function goToChrono(combo) {
+    target = combo.split(".");
+    if (target.length == 2) {
+        let id = target[0];
+        let v = target[1];
+        let lang = await getOptimalLanguage(combo);
+        window.location.href = ("/read/?id=" + id + "&v=" + v + "&lang=" + lang);
+    } else if (target.length == 3) {
+        let id = target[0];
+        let v = target[1];
+        let lang = target[2];
+        window.location.href = ("/read/?id=" + id + "&v=" + v + "&lang=" + lang);
+    } else {
+        alert("ERROR: Redirection failed. Please report to admin@wallofhistory.com.");
+        window.location.href = "/";
+    }
+}
+
+/* This function attempts to jump the user back to their most recent spot,
+prompting them to select a reading order if necessary. */
+async function jumpToChrono() {
+    if (sessionStorage.getItem("activeReadingOrder") === null) {
+        generateSelectionModal();
+    } else if ((sessionStorage.getItem("activeReadingOrder") != null) && (localStorage.getItem("savePlace:" + (sessionStorage.getItem("activeReadingOrder"))) === null)) {
+        let readingOrder = localStorage.getItem("readingOrder:" + sessionStorage.getItem("activeReadingOrder")).split(",");
+        let index, value;
+        for (index = 0; index < readingOrder.length; ++index) {
+            value = readingOrder[index];
+            if (value.substring(value.length - 2, value.length) === ":1") {
+                result = value;
+                newID = readingOrder[index].substring(0, readingOrder[index].indexOf(":")).split(".")[0];
+                newVersion = readingOrder[index].substring(0, readingOrder[index].indexOf(":")).split(".")[1];
+                newLang = await getOptimalLanguage(readingOrder[index].substring(0, readingOrder[index].indexOf(":")));
+                window.location.href = ("/read/?id=" + newID + "&v=" + newVersion + "&lang=" + newLang);
+                break;
+            }
+        }
+    } else {
+        window.location.href = localStorage.getItem("savePlace:" + (sessionStorage.getItem("activeReadingOrder")));
+    }
+}
+
+/* This function shows the relevant navigation buttons for a given page,
+relative to the active reading order. */
+function showButtonsChrono() {
     if (document.getElementById("backbutton") !== null) {
         let backButton = document.getElementById("backbutton");
         let forwardButton = document.getElementById("forwardbutton");
@@ -245,74 +320,36 @@ function showButtons() {
     }
 }
 
-let showInterval = window.setInterval(showCallback, 500);
-
-function showCallback() {
+// This function ensures the above function eventually completes correctly.
+let showInterval = window.setInterval(showChronoCallback, 500);
+function showChronoCallback() {
     // Find a better way to do this (probably involving async functions).
     // This might be necessary for the... reading order select thing.
     // If no reading order is selected, one NEEDS to be selected, THEN this can run.
-    showButtons();
+    showButtonsChrono();
 }
 
-/* LANGUAGE STUFF (INCOMPLETE) */
-
-let full = localStorage.getItem("languageList").split(",");
-let available = document.getElementsByTagName("section");
-
-if (available.length >= 1) {
-    let a = []
-    for (i = 0; i < available.length; i++) {
-        a.push(available[i].lang);
+/* This function saves a user’s place for the active reading order. */
+function savePlaceChrono() {
+    try {
+        localStorage.setItem("savePlace:" + sessionStorage.getItem("activeReadingOrder"), window.location);
+        alert("Your place was saved successfully.")
+    } catch {
+        alert("Your place was not saved successfully. Please try clearing your cache and trying again.")
     }
-
-    let intersection = full.filter(x => a.includes(x));
-
-    $("section:lang(" + intersection[0] + ")").css("display", "block");
 }
 
-/* READER NAVIGATION */
-
-async function goTo(combo) {
-    target = combo.split(".");
-    if (target.length == 2) {
-        let id = target[0];
-        let v = target[1];
-        let lang = await getOptimalLanguage(combo);
-        window.location.href = ("/read/?id=" + id + "&v=" + v + "&lang=" + lang);
-    } else if (target.length == 3) {
-        let id = target[0];
-        let v = target[1];
-        let lang = target[2];
-        window.location.href = ("/read/?id=" + id + "&v=" + v + "&lang=" + lang);
+/* This function attempts to jump the user back to their current saved place. */
+function loadPlaceChrono() {
+    if (localStorage.getItem("savePlace:" + sessionStorage.getItem("activeReadingOrder")) === null) {
+        jumpTo();
     } else {
-        alert("ERROR: Redirection failed. Please report to admin@wallofhistory.com.");
-        window.location.href = "/";
+        window.location.href = localStorage.getItem("savePlace:" + sessionStorage.getItem("activeReadingOrder"));
     }
 }
 
-async function jumpTo() {
-    if (sessionStorage.getItem("activeReadingOrder") === null) {
-        generateSelectionModal();
-    } else if ((sessionStorage.getItem("activeReadingOrder") != null) && (localStorage.getItem("savePlace:" + (sessionStorage.getItem("activeReadingOrder"))) === null)) {
-        let readingOrder = localStorage.getItem("readingOrder:" + sessionStorage.getItem("activeReadingOrder")).split(",");
-        let index, value;
-        for (index = 0; index < readingOrder.length; ++index) {
-            value = readingOrder[index];
-            if (value.substring(value.length - 2, value.length) === ":1") {
-                result = value;
-                newID = readingOrder[index].substring(0, readingOrder[index].indexOf(":")).split(".")[0];
-                newVersion = readingOrder[index].substring(0, readingOrder[index].indexOf(":")).split(".")[1];
-                newLang = await getOptimalLanguage(readingOrder[index].substring(0, readingOrder[index].indexOf(":")));
-                window.location.href = ("/read/?id=" + newID + "&v=" + newVersion + "&lang=" + newLang);
-                break;
-            }
-        }
-    } else {
-        window.location.href = localStorage.getItem("savePlace:" + (sessionStorage.getItem("activeReadingOrder")));
-    }
-}
-
-async function goBack() {
+/* This function navigates the user to the previous selected page in the active reading order. */
+async function goBackChrono() {
     let readingOrder = localStorage.getItem("readingOrder:" + sessionStorage.getItem("activeReadingOrder")).split(",");
     let currentNumber = findSelf();
     for (index = currentNumber - 1; index < readingOrder.length; index--) {
@@ -324,7 +361,8 @@ async function goBack() {
     }
 }
 
-async function goForward() {
+/* This function navigates the user to the next selected page in the active reading order. */
+async function goForwardChrono() {
     let readingOrder = localStorage.getItem("readingOrder:" + sessionStorage.getItem("activeReadingOrder")).split(",");
     let currentNumber = findSelf();
     for (index = currentNumber + 1; index < readingOrder.length; index++) {
@@ -336,25 +374,7 @@ async function goForward() {
     }
 }
 
-function savePlace() {
-    try {
-        localStorage.setItem("savePlace:" + sessionStorage.getItem("activeReadingOrder"), window.location);
-        alert("Your place was saved successfully.")
-    } catch {
-        alert("Your place was not saved successfully. Please try clearing your cache and trying again.")
-    }
-}
-
-function loadPlace() {
-    if (localStorage.getItem("savePlace:" + sessionStorage.getItem("activeReadingOrder")) === null) {
-        jumpTo();
-    } else {
-        window.location.href = localStorage.getItem("savePlace:" + sessionStorage.getItem("activeReadingOrder"));
-    }
-}
-
-/* READ AS STANDALONE */
-
+/* Helper function for readAsStandalone (interfaces with necessary backend code). */
 function readAsStandaloneSetup(id) {
     return new Promise(resolve => {
         var xmlhttp = new XMLHttpRequest();
@@ -368,6 +388,7 @@ function readAsStandaloneSetup(id) {
     });
 }
 
+/* Calls readAsStandaloneSetup(), sets necessary localStorage variables, and jumps the user to the first page. */
 async function readAsStandalone() {
     let currentID = document.getElementById("downloadMarker").innerHTML;
     let newOrder = await readAsStandaloneSetup(currentID);
@@ -375,3 +396,121 @@ async function readAsStandalone() {
     sessionStorage.setItem("activeReadingOrder", currentID);
     jumpTo();
 }
+
+/****************************************************
+ * END CHRONOLOGY-BASED READER NAVIGATION FUNCTIONS *
+ ****************************************************/
+
+
+/*****************************************************************************
+ * SIMPLE TREE-BASED READER NAVIGATION FUNCTIONS (MYTHS AND LEGACY VERSIONS) *
+ *****************************************************************************/
+
+/* This function gets an array of the siblings of the current page. */
+function getArrayTree() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let id = urlParams.get("id");
+
+    return new Promise(resolve => {
+        let xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                resolve(this.responseText);
+            }
+        };
+        xmlhttp.open("GET", "/php/getarray.php?q=" + id, true);
+        xmlhttp.send();
+    });
+}
+
+/* This function finds the index of the current page in the aforementioned array. */
+async function findSelfTree() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let id = urlParams.get("id");
+    let siblings = await getArrayTree();
+
+    return siblings.indexOf(id);
+}
+
+/* This function shows the relevant navigation buttons for a given page. */
+async function showButtonsTree() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let id = urlParams.get("id");
+    let siblings = await getArrayTree();
+    siblings = siblings.split(",");
+
+    if (id == siblings[0]) {
+        let saveButtons = document.getElementsByClassName("savefile");
+        saveButtons[0].style.display = "flex";
+        let forwardButton = document.getElementById("forwardbutton");
+        forwardButton.style.display = "block";
+    } else if (id == siblings[siblings.length - 1]) {
+        let saveButtons = document.getElementsByClassName("savefile");
+        saveButtons[0].style.display = "flex";
+        let backButton = document.getElementById("backbutton");
+        backButton.style.display = "block";
+    } else if (siblings.includes(id)) {
+        let saveButtons = document.getElementsByClassName("savefile");
+        saveButtons[0].style.display = "flex";
+        let backButton = document.getElementById("backbutton");
+        let forwardButton = document.getElementById("forwardbutton");
+        backButton.style.display = "block";
+        forwardButton.style.display = "block";
+    }
+}
+
+/* This function is called when the page is loaded.
+Uh… Find a better option than just calling it raw, I guess. */
+showButtonsTree();
+
+/* This function saves the user’s place.
+TO-DO: Make this parent-specific, and add a selector if the load button is used on the homepage. */
+function savePlaceTree() {
+    localStorage.setItem("MythsandLegacySavePlace", window.location);
+    alert("Your place was saved successfully.");
+}
+
+/* This function attempts to jump the user back to their most recent spot.
+TO-DO: Make this parent-specific, and add a selector is the load button is used on the homepage.
+Also, add an option for users to “finish” things and clear them from the reading orders. */
+function jumpToTree() {
+    if (localStorage.getItem("MythsandLegacySavePlace") === null) {
+        window.location.pathname = "/read";
+    } else {
+        window.location.href = localStorage.getItem("MythsandLegacySavePlace");
+    }
+}
+
+/* This function attempts to jump the user back to their current saved place.
+TO-DO: Make this parent-specific, and add a selector is the load button is used on the homepage. */
+function loadPlaceTree() {
+    if (localStorage.getItem("MythsandLegacySavePlace") === null) {
+        jumpToTree();
+    } else {
+        window.location.href = localStorage.getItem("MythsandLegacySavePlace");
+    }
+}
+
+/* This function navigates the user to the previous selected page in the current array. */
+async function goBackTree() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let id = urlParams.get("id");
+    let siblings = await getArrayTree();
+    siblings = siblings.split(",");
+    let position = siblings.indexOf(id);
+    window.location.href = "/read/?id=" + siblings[position-1];
+}
+
+/* This function navigates the user to the next selected page in the current array. */
+async function goForwardTree() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let id = urlParams.get("id");
+    let siblings = await getArrayTree();
+    siblings = siblings.split(",");
+    let position = siblings.indexOf(id);
+    window.location.href = "/read/?id=" + siblings[position+1];
+}
+
+/*****************************************************
+ * END SIMPLE TREE-BASED READER NAVIGATION FUNCTIONS *
+ *****************************************************/
--- js/mnl/palette.js
+++
@@ -1,8 +0,0 @@
-function swapPalettes() {
-    console.log("Swapping palettes…");
-    const root = document.documentElement;
-    root.classList.add('white');
-
-    document.documentElement.classList.toggle('light');
-    root.classList.toggle('black');
-}
