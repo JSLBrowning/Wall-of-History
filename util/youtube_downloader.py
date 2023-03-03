aef73c58aa6b3257a6d74456f44df2f370eb4de3
James Browning <james@wallofhistory.com>
2023-03-02 23:32:05 -0600

    Add HD YouTube downloader utility

---
+++ util/youtube_downloader.py
@@ -0,0 +1,19 @@
+from pytube import YouTube
+
+
+def download_yt(url):
+    yt = YouTube(url)
+    # Get highest-quality video and save as an MP4.
+    yt.streams.filter(only_audio=False, progressive=False, file_extension="mp4") \
+        .order_by("resolution").desc().first().download(output_path="out/yt/", filename=yt.title + "_video.mp4")
+    # Get highest-quality audio and save as an MP3.
+    yt.streams.get_audio_only().download(output_path="out", filename=yt.title + "_audio.mp3")
+
+
+def main(url_list):
+    for url in url_list:
+        download_yt(url)
+
+
+main(["https://youtu.be/PqLy1tgyphw",
+      "https://youtu.be/oxouZkrrqUc"])
